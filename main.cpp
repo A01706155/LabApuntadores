1883fbb577526450edf65ac0cc8baa829c5e00a3
A01706155 <A01706155@itesm.mx>
2020-05-30 03:14:13 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <string.h> 
+#include "sumador.h"
+  
+using namespace std; 
+
+int main() 
+{ 
+
+    Sumador a("32940835902858459273489", "849759357083450484");
+    cout << a.getSuma();
+    return 0; 
+} 
---
+++ sumador.h
@@ -0,0 +1,32 @@
+#ifndef Sumador_h
+#define Sumador_h
+
+#include <string>
+
+using namespace std;
+
+// Clase padre Sumador:
+class Sumador {  
+  protected:
+   string numero1,numero2;
+
+
+public:
+  Sumador();
+  Sumador(string numerote1, string numerote2);
+  string getSuma();
+  string elnum1, elnum2;
+  string dir_num1, dir_num2;
+
+};
+
+Sumador::Sumador(string nu1, string nu2) {
+  elnum1 = nu1;
+  elnum2 = nu2;
+}
+
+string Sumador::getSuma() {
+  return elnum1;
+}
+
+#endif
